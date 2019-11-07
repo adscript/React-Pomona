b457f88460e3fe0c7b3cc5b19401461c2256b708
adscript <adnanocode@gmail.com>
2019-11-07 23:33:13 +0700

    complete login, register, logout action

---
+++ src/actions/auth.js
@@ -0,0 +1,76 @@
+// import { push } from "connected-react-router";
+// import { loadDetail } from "./detail";
+
+const API_URL = "https://pomonatodo.herokuapp.com";
+
+const loginSuccess = (token) => ({
+    type: 'LOGIN_SUCCESS',
+    token
+})
+
+const registerError = (message) => ({
+    type: 'REGISTER_ERROR',
+    message
+})
+
+const loginError = (message) => ({
+    type: 'LOGIN_ERROR',
+    message
+})
+
+//START REGISTER
+export const postRegister = user => {
+    return dispatch => {
+        return fetch(`${API_URL}/auth/register`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message) {
+                    dispatch(registerError(data.data.message));
+                } else {
+                    localStorage.setItem("token", data.data.token);
+                    dispatch(loginSuccess(data.data.token));
+                }
+            }).catch(err => console.log(err))
+    }
+}
+
+// START LOGIN
+export const postLogin = user => {
+    return dispatch => {
+        return fetch(`${API_URL}/auth/login`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.data.message) {
+                    dispatch(loginError(data.data.message));
+                } else {
+                    localStorage.setItem("token", data.data.token)
+                    dispatch(loginSuccess(data.data.token))
+                }
+            }).catch(err => console.log(err))
+    }
+}
+
+// START LOGOUT
+export const logoutSuccess = () => ({
+    type: 'LOGOUT_SUCCESS'
+})
+
+
+
+
+
+
